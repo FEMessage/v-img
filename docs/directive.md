cb4866fae662425845810cdfd56a946b9ed3cb41
colmugx <colmugx@qq.com>
2019-11-12 20:35:04 +0800

    fix: 不需要background配置

--- docs/directive.md
+++ docs/directive.md
@@ -2,7 +2,6 @@
 
 - 图片地址依然使用 `src` 参数
 - 与 v-img 一样的参数设置，如 `v-img:background="{provider: 'qiniu'}"`
-- 支持 `background` 参数，如 `v-img:background="{size: 'cover'}"`
 
 
 ```vue
--- src/background.js
+++ src/background.js
@@ -2,31 +2,27 @@ import 'lazysizes/plugins/bgset/ls.bgset'
 import providerConf from './provider-config'
 
 const isBg = arg => arg === 'background'
-const isSupportWebp = JSON.parse(localStorage.getItem('isSupportWebp')) || false
+
+function getSrc(el, config) {
+  const isSupportWebp =
+    JSON.parse(localStorage.getItem('isSupportWebp')) || false
+  const {provider = 'alibaba', extraQuery} = config
+  const src = providerConf[provider].getSrc({
+    src: el.getAttribute('src'),
+    isSupportWebp,
+    extraQuery
+  })
+
+  return src
+}
 
 export default {
   init(el, {arg, value = {}}) {
     if (!isBg(arg)) {
       return
     }
-    const {provider = 'alibaba', extraQuery, ...other} = value
-    const backgroundArgs = Object.keys(other).filter(
-      item => `background${item[0].toUpperCase() + item.slice(1)}` in el.style
-    )
-    const src = providerConf[provider].getSrc({
-      src: el.getAttribute('src'),
-      isSupportWebp,
-      extraQuery
-    })
 
-    el.style.backgroundSize = 'cover'
-    if (backgroundArgs.length) {
-      backgroundArgs.forEach(arg => {
-        const key = arg[0].toUpperCase() + arg.slice(1)
-        el.style[`background${key}`] = other[arg]
-      })
-    }
-
+    const src = getSrc(el, value)
     el.classList.add('lazyload')
     el.setAttribute('data-bgset', src)
   },
@@ -35,13 +31,8 @@ export default {
     if (!isBg(arg)) {
       return
     }
-    const {provider = 'alibaba', extraQuery} = value
-    const src = providerConf[provider].getSrc({
-      src: el.getAttribute('src'),
-      isSupportWebp,
-      extraQuery
-    })
 
+    const src = getSrc(el, value)
     el.style.backgroundImage = `url(${src})`
   }
 }
