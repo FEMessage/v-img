9f0581f85a60813f6e5890ebd57bb2bb3ace00e1
Han <xsytby1112@gmail.com>
2020-01-17 16:39:34 +0800

    style: 修改 placeholder 背景颜色

--- src/v-img.vue
+++ src/v-img.vue
@@ -113,7 +113,7 @@ export default {
            * 图片较大时，loading 的圈圈则固定大小
            */
           return {
-            backgroundColor: '#eeedeb',
+            backgroundColor: '#F0F2F5',
             objectFit: 'scale-down',
             cursor: 'pointer'
           }
