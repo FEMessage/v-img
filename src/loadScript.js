9c365231935327784f89873bc72c2b4f5a90153f
Han <xsytby1112@gmail.com>
2020-06-12 17:35:14 +0800

    fix: 记录 loaded script

--- src/loadScript.js
+++ src/loadScript.js
@@ -1,10 +1,16 @@
+const loaded = new Set()
+
 export default function(script) {
   return new Promise((resolve, reject) => {
     if (!script) {
       reject(new Error('Script is not found'))
     }
+    if (loaded.has(script)) {
+      resolve('loaded')
+    }
     const loadScript = document.createElement('script')
     loadScript.onload = function() {
+      loaded.add(script)
       resolve(true)
     }
     loadScript.onerror = function(err) {
