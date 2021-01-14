25791d61ff41256f690496b5e0bf323b4cc91fbc
levy <897895407@qq.com>
2021-01-14 15:34:41 +0800

    fix: 处理变形或溢出情况

--- src/compute-layout.js
+++ src/compute-layout.js
@@ -4,6 +4,7 @@
 function isPixel(dimension) {
   return ('' + dimension).indexOf('px') > -1
 }
+
 export default function(vm) {
   let width, height
   if (Number.isInteger(vm.width) || isPixel(vm.width))
@@ -11,28 +12,31 @@ export default function(vm) {
   if (Number.isInteger(vm.height) || isPixel(vm.height))
     height = parseInt(vm.height)
 
-  if (!isNaN(width) && !isNaN(height)) return {width, height}
-
   if (isNaN(width) && vm.$el) {
     let styleWidth = window.getComputedStyle(vm.$el).width
-
-    if (styleWidth.indexOf('px') > -1) width = parseInt(styleWidth)
-    else {
-      width = (height * vm.$el.naturalWidth) / vm.$el.naturalHeight
-    }
+    if (isPixel(styleWidth)) width = parseInt(styleWidth)
   }
   if (isNaN(height) && vm.$el) {
     let styleHeight = window.getComputedStyle(vm.$el).height
-
-    if (styleHeight.indexOf('px') > -1) height = parseInt(styleHeight)
-    else {
-      height = (width * vm.$el.naturalHeight) / vm.$el.naturalWidth
-    }
+    if (isPixel(styleHeight)) height = parseInt(styleHeight)
   }
 
-  let result = {}
-  if (width > 0) result.width = width
-  if (height > 0) result.height = height
+  // TODO 这里假设 auto 没有返回数值，待验证
+  if ((isNaN(width) && isNaN(height)) || (!width && !height)) return {}
 
-  return result
+  let naturalWidth = vm.$el.naturalWidth,
+    naturalHeight = vm.$el.naturalHeight
+
+  if (width > naturalWidth) width = naturalWidth
+  if (height > naturalHeight) height = naturalHeight
+
+  // getComputedStyle 有可能会返回 natural[Width/Height]，这使得其返回值不可直接使用，还需要最后 resize 一下。
+  let scaleWidth = (height * vm.$el.naturalWidth) / vm.$el.naturalHeight
+  let scaleHeight = (width * vm.$el.naturalHeight) / vm.$el.naturalWidth
+
+  if (!scaleWidth || width * scaleHeight <= height * scaleWidth)
+    return {width, height: scaleHeight}
+  else if (!scaleHeight || scaleWidth * height <= scaleHeight * width)
+    return {width: scaleWidth, height}
+  return {}
 }
--- test/compute-layout.spec.js
+++ test/compute-layout.spec.js
@@ -5,14 +5,15 @@ import computeLayout from '../src/compute-layout'
 describe('compute img layout', () => {
   let vm
   let expected
+  let natural
 
   beforeEach(() => {
     vm = {
       width: '', // 理想值是数字
       height: '', // 理想值是数字
       $el: {
-        naturalWidth: '', // 数字
-        naturalHeight: '', // 数字
+        naturalWidth: 200,
+        naturalHeight: 200,
         style: {
           width: '', // 可能是 auto, 可能是 100%，可能是 300px 或其他单位。。。
           height: '',
@@ -20,6 +21,21 @@ describe('compute img layout', () => {
       },
     }
     expected = {width: 100, height: 100}
+    natural = {width: 200, height: 200}
+  })
+
+  // https://developer.mozilla.org/en-US/docs/Web/API/Window/getComputedStyle#notes
+  // width, height, they are the same as used values
+  Object.defineProperty(window, 'getComputedStyle', {
+    value: jest.fn(el => {
+      let width = el.style.width || el.naturalWidth
+      let height = el.style.height || el.naturalHeight
+
+      return {
+        width: Number.isInteger(width) ? width + 'px' : width,
+        height: Number.isInteger(height) ? height + 'px' : height,
+      }
+    }),
   })
 
   // 此时优先使用 width
@@ -49,26 +65,15 @@ describe('compute img layout', () => {
 
   test('width 有数值，height/style.height 无值, height 自动计算', () => {
     vm.width = 100
-    vm.$el.naturalWidth = 200
-    vm.$el.naturalHeight = 200
     expect(computeLayout(vm)).toStrictEqual(expected)
   })
 
   test('style.width 有px值，height/style.height 无值, height 自动计算', () => {
     vm.$el.style.width = '100px'
-    vm.$el.naturalWidth = 200
-    vm.$el.naturalHeight = 200
     expect(computeLayout(vm)).toStrictEqual(expected)
   })
 
   test('只有 naturalWidth/naturalHeight', () => {
-    Object.defineProperty(window, 'getComputedStyle', {
-      value: jest.fn(el => ({
-        width: el.naturalWidth + 'px',
-        height: el.naturalHeight + 'px',
-      })),
-    })
-
     vm.$el.naturalWidth = 100
     vm.$el.naturalHeight = 100
     expect(computeLayout(vm)).toStrictEqual(expected)
@@ -77,15 +82,41 @@ describe('compute img layout', () => {
   test('width/height 不是纯数字, 暂不支持', () => {
     vm.width = '100%'
     vm.height = 'auto'
-    expect(computeLayout(vm)).toStrictEqual({})
+    expect(computeLayout(vm)).toStrictEqual(natural)
   })
 
   test('style.width/style.height 不是px，暂不支持', () => {
     vm.$el.style.width = 'auto'
     vm.$el.style.height = '100%'
     expect(computeLayout(vm)).toStrictEqual({})
+  })
 
-    vm.$el.style.width = '10em'
+  test('width/height 设置为 0, 忽略不计', () => {
+    vm.width = 0
+    vm.height = 0
     expect(computeLayout(vm)).toStrictEqual({})
+
+    vm.width = ''
+    vm.height = ''
+    vm.$el.style.width = '0'
+    vm.$el.style.height = '0'
+    expect(computeLayout(vm)).toStrictEqual({})
+  })
+
+  test('设置了不恰当比例的宽高，要防止变形', () => {
+    vm.$el.style.width = '80px'
+    vm.$el.style.height = '100px'
+    expect(computeLayout(vm)).toStrictEqual({width: 80, height: 80})
+
+    vm.width = 100
+    vm.height = 150
+    expect(computeLayout(vm)).toStrictEqual(expected)
+
+    vm.width = 300
+    vm.height = 300
+    expect(computeLayout(vm)).toStrictEqual(natural)
+
+    vm.width = 100
+    expect(computeLayout(vm)).toStrictEqual(expected)
   })
 })
